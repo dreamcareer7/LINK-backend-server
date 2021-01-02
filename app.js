4bcdaa89093d91e62e02b02f499dc60a415a055c
dreamcareer7 <dreamcareer070@gmail.com>
2021-01-02 16:29:46 +0530

    Allowed Access-Control-Allow-Headers headers

--- app.js
+++ app.js
@@ -35,6 +35,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'upload')));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
+
 app.use(
     cors({
         origin: true,
