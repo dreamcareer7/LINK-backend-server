ca8a10c49d477f26e9b3c499ec54fa1de2152fea
dreamcareer7 <dreamcareer070@gmail.com>
2021-01-18 15:28:19 +0530

    Changed the Sign-in redirection

--- routes/client-auth.route.js
+++ routes/client-auth.route.js
@@ -67,10 +67,11 @@ router.get('/sign-up', async (req, res) => {
             await client.save();
             console.log('Client Token ::', token);
             Logger.log.info('Login Successfully to Client Deshbord.');
-            return res.status(200).send({
-                message: 'Welcome to Dashbord.',
-                status: 'SUCCESS',
-            });
+            // return res.status(200).send({
+            //     message: 'Welcome to Dashbord.',
+            //     status: 'SUCCESS',
+            // });
+            return res.redirect(`${config.clientUrls.clientFrontEndBaseUrl}auth-verify?token=${token}`);
         }
     } catch (e) {
         Logger.log.error('Error in SignUp API call.', e.message || e);
