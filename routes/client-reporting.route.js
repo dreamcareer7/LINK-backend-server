31a4ac7615ccdd2d65755dbf06aa8f6c5542561d
dreamcareer7 <dreamcareer070@gmail.com>
2021-01-26 13:36:22 +0530

    Total Sales Graph

--- routes/client-reporting.route.js
+++ routes/client-reporting.route.js
@@ -75,6 +75,7 @@ router.get('/activity-breakdown', async (req, res) => {
         });
     }
 });
+
 router.get('/pipeline-value', async (req, res) => {
     try {
         if (!req.query.startDate || !req.query.endDate) {
@@ -110,6 +111,68 @@ router.get('/pipeline-value', async (req, res) => {
         data = data.filter(function(value, index, arr) {
             return value._id !== null;
         });
+        return res.status(200).send({
+            status: 'SUCCESS',
+            data: data,
+        });
+    } catch (e) {
+        Logger.log.error('Error in client-dashboard pipeline-value API call', e.message || e);
+        res.status(500).json({
+            status: 'ERROR',
+            message: e.message,
+        });
+    }
+});
+
+router.get('/total-sales', async (req, res) => {
+    try {
+        if (!req.query.startDate || !req.query.endDate) {
+            return res.status(400).send({
+                status: 'ERROR',
+                message: 'Start Date or End Date not found',
+            });
+        }
+        let startDate = new Date(req.query.startDate);
+        let endDate = new Date(req.query.endDate);
+        let data = await Opportunity.aggregate([
+            [
+                {
+                    $match: {
+                        clientId: req.client._id,
+                        isDeleted: false,
+                        createdAt: { $gte: startDate, $lte: endDate },
+                        stage: 'CLOSED',
+                    },
+                },
+                {
+                    $group: {
+                        _id: {
+                            month: {
+                                $month: {
+                                    date: '$createdAt',
+                                },
+                            },
+                            day: {
+                                $dayOfMonth: {
+                                    date: '$createdAt',
+                                },
+                            },
+                            year: {
+                                $year: {
+                                    date: '$createdAt',
+                                },
+                            },
+                        },
+                        total: {
+                            $sum: 1,
+                        },
+                        totalDealValue: {
+                            $sum: '$dealSize',
+                        },
+                    },
+                },
+            ],
+        ]).allowDiskUse(true);
         return res.status(200).send({
             status: 'SUCCESS',
             data: data,
