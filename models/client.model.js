4147eded5ccc9599b8ee0fd366d8677ccfc9d8fa
dreamcareer7 <dreamcareer070@gmail.com>
2021-02-08 12:23:45 +0530

    Reomved enum for company size

--- models/client.model.js
+++ models/client.model.js
@@ -28,7 +28,6 @@ const clientSchema = new Schema(
         linkedInUrl: Schema.Types.String,
         companySize: {
             type: Schema.Types.String,
-            enum: ['0-1', '2-10', '11-50', '51-200', '201-500', '501-1,000', '1,001-5,000', '5,001-10,000', '10,001+'],
         },
         linkedInID: Schema.Types.String,
         publicIdentifier: Schema.Types.String,
